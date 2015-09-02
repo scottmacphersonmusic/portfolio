54d6ed29bb8304cec106455a7552007a94938596
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-01 18:04:25 -0700

    Revise feature spec

--- test/features/can_visit_welcome_test.rb
+++ test/features/can_visit_welcome_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class WelcomePageTest < Capybara::Rails::TestCase
   test "welcome page should exist" do
     visit root_path
-    assert_content page, "Hello World"
+    assert_content page, "Scott Macpherson"
     refute_content page, "Goobye All!"
   end
 end
