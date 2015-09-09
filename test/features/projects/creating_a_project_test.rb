29ca32077e575a1782f748aa709aae74946ec015
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-08 17:45:58 -0700

    Create project spec from errors to failure

--- test/features/projects/creating_a_project_test.rb
+++ test/features/projects/creating_a_project_test.rb
@@ -6,7 +6,7 @@ feature "As the site owner, I want to add a portfolio item so that I can show
     visit projects_path
     click_on "New Project"
     fill_in "Name", with: "Super Cool App"
-    fill_in "Technologies Used", with: "Rails, Ruby, Foundation, HTML5, CSS3"
+    fill_in "Technologies used", with: "Rails, Ruby, Foundation, HTML5, CSS3"
     click_on "Create Project"
     page.text.must_include "Project has been created"
     assert page.has_css?("#notice"), "Expected a flash notice - none found"
