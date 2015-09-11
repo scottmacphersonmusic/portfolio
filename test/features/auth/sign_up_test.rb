2a8a4b8d62e858ce583fdca6a542a72ad82b5baa
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-10 21:26:16 -0700

    Rename to match format

--- test/features/auth/sign_up_test.rb
+++ test/features/auth/sign_up_test.rb
@@ -4,7 +4,7 @@ feature "As a site owner, I want to be able to sign up for an account,
 so that I can access features restricted to those who are signed in" do
   scenario "sign up" do
     # Given a signup form, When I submit valid info
-    signup
+    sign_up
     # Then I should be signed up
     page.must_have_content "Welcome! You have signed up successfully."
   end
