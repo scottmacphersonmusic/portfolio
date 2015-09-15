a01a95e5df0b3888950d55bd1c0eaaaa5a513ea5
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-15 09:02:33 -0700

    Fix Zurb theme/template tests

--- test/features/theme_is_present_test.rb
+++ test/features/theme_is_present_test.rb
@@ -1,21 +1,14 @@
 require "test_helper"
 
-feature "Theme Is Present" do
-  scenario "the expected stylesheets are present" do
+feature "Theme/Template Is Present" do
+  scenario "zurb css classes are present" do
     visit root_path
-    save_and_open_page
-    base = "/assets/"
-    styles = %w(app foundation foundation_and_overrides scaffolds)
-    styles.each { |s| page.html.must_have_content base + s }
+    classes = %w(nav.top-bar ul.title-area div.hero div.row div.columns)
+    classes.each { |c| page.must_have_css(c) }
   end
 
   scenario "the expected javascript files are present" do
     visit root_path
-    base = "jquery.foundation."
-    a = %w(accordion.js alerts.js buttons.js clearing.js forms.js joyride.js)
-    b =  %w(magellan.js mediaQueryToggle.js navigation.js orbit.js reveal.js)
-    c =  %w(tabs.js tooltips.js topbar.js)
-    js = a + b + c
-    js.each { |s| page.html.must_include base + s }
+    page.html.must_include "modernizr"
   end
 end
