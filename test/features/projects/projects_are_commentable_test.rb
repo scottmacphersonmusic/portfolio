9e05d6fc2088bc71ebe339bf51d0642d02c68934
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-27 21:07:28 -0700

    Pass project visitor comments tests
    Make approved true on project comment fixture

--- test/features/projects/projects_are_commentable_test.rb
+++ test/features/projects/projects_are_commentable_test.rb
@@ -12,53 +12,52 @@ feature "visitors have limited crud actions on project comments" do
     fill_in 'Commenter email', with: "troll@example.com"
     fill_in 'Content', with: "Troll like project"
     click_on "Submit Comment"
-    save_and_open_page
     # Then a new comment is submitted for approval
     page.must_have_content "Comment has been submitted to the editor for approval."
     # redirected to project, which doens't have comment yet
     page.wont_have_content "Troll like project"
   end
 
-  # scenario "Visitors should only see approved comments" do
-  #   # Given existing comments for an article
-  #   # When I visit an article's show page
-  #   visit article_path(articles(:rails))
-  #   # Then the unapproved comment should not be there
-  #   page.must_have_content "The Dude"
-  # end
+  scenario "Visitors should only see approved comments" do
+    # Given existing comments for a project
+    # When I visit an project's show page
+    visit project_path(projects(:hack))
+    # Then the unapproved comment should not be there
+    page.must_have_content "The Dude"
+  end
 
   # - - - Unhappy Paths - - -
 
-  # scenario "Visitors can't create invalid comments" do
-  #   # Given a new article_comment form
-  #   visit new_article_comment_path(articles(:rails))
-  #   # When I submit invalid comment data
-  #   fill_in "Commenter name", with: ""
-  #   fill_in "Commenter email", with: ""
-  #   fill_in "Content", with: ""
-  #   click_on "Submit Comment"
-  #   # Then no comment is submitted for approval
-  #   page.must_have_content "3 errors prohibited this comment from being saved:"
-  #   page.must_have_content "Commenter name can't be blank"
-  #   page.must_have_content "Commenter email can't be blank"
-  #   page.must_have_content "Content can't be blank"
-  # end
+  scenario "Visitors can't create invalid comments" do
+    # Given a new project_comment form
+    visit new_project_comment_path(projects(:hack))
+    # When I submit invalid comment data
+    fill_in "Commenter name", with: ""
+    fill_in "Commenter email", with: ""
+    fill_in "Content", with: ""
+    click_on "Submit Comment"
+    # Then no comment is submitted for approval
+    page.must_have_content "3 errors prohibited this comment from being saved:"
+    page.must_have_content "Commenter name can't be blank"
+    page.must_have_content "Commenter email can't be blank"
+    page.must_have_content "Content can't be blank"
+  end
 
-  # scenario "Visitors can't update comments" do
-  #   # When I try to visit an edit article form
-  #   visit edit_article_comment_path(articles(:rails), comments(:dude_on_rails))
-  #   # Then I am redirected and see a message
-  #   page.must_have_content "You need to sign in or sign up before continuing."
-  #   page.wont_have_content "Edit Comment"
-  # end
+  scenario "Visitors can't update comments" do
+    # When I try to visit an edit comment form
+    visit edit_project_comment_path(projects(:hack), comments(:dude_on_hack))
+    # Then I am redirected and see a message
+    page.must_have_content "You need to sign in or sign up before continuing."
+    page.wont_have_content "Edit Comment"
+  end
 
-  # scenario "Visitors wont have edit or delete links available" do
-  #   # When I view an article page with an existing comment
-  #   visit article_path(articles(:rails))
-  #   # Then I wont have access to edit or delete links
-  #   page.wont_have_link "Edit"
-  #   page.wont_have_link "Delete"
-  # end
+  scenario "Visitors wont have edit or delete links available" do
+    # When I view an project page with an existing comment
+    visit project_path(projects(:hack))
+    # Then I wont have access to edit or delete links
+    page.wont_have_link "Edit"
+    page.wont_have_link "Delete"
+  end
 end
 
 # feature "Editors have crud actions for comments" do
