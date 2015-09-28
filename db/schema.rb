ef1a6704ea8c6bb9fb5f1fb1c57b1de482aeeba4
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-27 20:06:23 -0700

    Give comments table approved default value of false

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926191618) do
+ActiveRecord::Schema.define(version: 20150928023919) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 20150926191618) do
     t.string   "user_agent"
     t.string   "referrer"
     t.text     "content"
-    t.boolean  "approved"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.boolean  "approved",         default: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.integer  "commentable_id"
     t.string   "commentable_type"
   end
