addf63a59faa865d02a16b593112553b9b3b425d
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-29 00:08:32 -0700

    Remove unwanted controller/mailer
    Update mailer test

--- app/mailers/contact_mailer.rb
+++
@@ -1,7 +0,0 @@
-class ContactMailer < ApplicationMailer
-  def contact_owner(user)
-    mail to: "scottmacphersonmusic@gmail.com",
-         subject: "Portfolio Message",
-         from: user.email
-  end
-end
--- test/features/contact_delivers_mail_test.rb
+++ test/features/contact_delivers_mail_test.rb
@@ -1,23 +1,20 @@
 require "test_helper"
 
-feature "Contact delivers mail" do
+feature "Email the user notifications" do
   include EmailSpec::Helpers
   include EmailSpec::Matchers
 
-  scenario "visitors can email me through the contact page" do
-    # Given a contact form
-    visit contacts_path
-    # When a visitor fills out the required fields
-    fill_in "Name", with: "John Coltrane"
-    fill_in "Email", with: "trane@example.com"
-    fill_in "Message", with: "a love supreme"
-    email = ContactMailer.contact_owner
-    # And clicks 'Send'
-    click_on "Send"
-    # Then an email should be delivered to me
-    email.must reply_to("trane@example.com")
-    email.must deliver_to "scottmacphersonmusic@gmail.com"
-    email.must have_subject "Portfolio Message"
-    email.must have_body_text "John Coltrane says:"
+  scenario "authors receive an email when their article are published" do
+    sign_in
+    # Given an unpublished article
+    visit edit_article_path articles(:unpublished)
+    # When the article is published
+    check "Published"
+    click_on "Update Article"
+    # Then the author should be notified by email
+    email.must reply_to("scottmacphersonmusic@gmail.com")
+    email.must deliver_to "author@example.com"
+    email.must have_subject "Article Published"
+    email.must have_body_text "your article has been published"
   end
 end
