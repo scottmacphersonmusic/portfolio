322f6f77d3ceb74908fedc5db74e096ca7ef737f
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-30 15:05:05 -0700

    Current state of mayham
    Add carrierwave-direct, fog, sidekiq, redis

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -1,20 +1,23 @@
 # encoding: utf-8
+require 'carrierwave/processing/mini_magick'
 
-class ProfileLogoUploader < CarrierWave::Uploader::Base
-
+class LogoUploader < CarrierWave::Uploader::Base
+  include CarrierWaveDirect::Uploader
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  # include CarrierWave::RMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :fog    # not necessary with carrierwave-direct
+
+  include CarrierWave::MimeTypes
+  process :set_content_type
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end   # not necessary with carrierwave-direct
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -25,27 +28,26 @@ class ProfileLogoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fit => [200, 200]
+  # process :resize_to_fit => [200, 200]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :display do
-  #   process :resize_to_fit => [150, 150]
-  # end
+  version :display do
+    process :resize_to_limit => [200, 200]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
-
 end
