3eb90e4507c1af025d584bf73470e3495309c40c
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-10-05 22:39:44 -0700

    Configure mailing environments

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,5 +1,16 @@
-ActionMailer::Base.smtp_settings = {
-  address: "scottmacphersonmusic",
-  port:  3000,
-  domain: "localhost"
-}
+unless Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.example.com",
+    port:  3000,
+    domain: "localhost"
+  }
+else
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
+end
